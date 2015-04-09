488b4e832101691464da11bbf2ad44d415d364a0
James Tan <james.tan@nih.gov>
2015-04-09 16:16:40 -0400

    JR208 Added test runner

---
+++ cadsrapi/software/SDK/common/customizations/java/gov/nih/nci/cadsrapi/util/TestRunner.java
@@ -0,0 +1,34 @@
+package gov.nih.nci.cadsrapi.util;
+
+import gov.nih.nci.cadsrapi.dao.orm.CleanerDAO;
+
+/**
+ * This is just a test runner and should not be used in production.
+ * @comment created specifically for https://tracker.nci.nih.gov/browse/CADSRAPI-208
+ */
+public class TestRunner {
+	private static int sleepTime = 60;
+
+	public static void main(String[] args) {
+		CleanerDAO cleaner = new CleanerDAO();
+		try {
+			int temp = Integer.valueOf(PropertiesLoader.getProperty("cart.cleaner.sleep.minutes"));  //Get property value
+			sleepTime = temp;  //set to sleep time
+		} catch (Exception ae) {
+			ae.printStackTrace();
+		}
+		
+		while (true) {
+			try {
+				System.out.println("Cleaner running");
+				cleaner.clean();
+				System.out.println("Cleaner stopped");
+				Thread.sleep(sleepTime*60*1000);  //Put thread to sleep, converting minutes to milliseconds
+			} catch (InterruptedException ie){
+				System.out.println(ie.getMessage());
+				ie.printStackTrace();	
+			}		
+		}
+		
+	}
+}
